91566d4c715382bd621465e1dffae78b5d476686
Guilherme Carvalho <guilherme.gomes@afya.com.br>
2023-05-29 12:11:54 -0300

    atualização do software

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
-const nomes = [ "Ilso", "Ediana", "Eliana", "Paulo","carlinho", "Leny", "Beto", "Sergio" ];
-const storageKey = "proximaPessoa";
-const nomeAtualEl = document.getElementById("nome-atual");
-const nomeProximoEl = document.getElementById("nome-proximo");
+const schedule = ['Paulo','Carlinhos', 'Leny', 'Beto', 'Sergio', 'Ilson', 'Ediana', 'Eliana' ];
 
-function atualizarEscalonamento() {
-  const hoje = new Date();
-  const proximaData = new Date(hoje);
-  proximaData.setDate(hoje.getDate() + 1);
-  const proximaPessoaIndex = proximaData.getDate() % nomes.length;
+function updateSchedule() {
+  const now = new Date();
+  const currentPersonIndex = now.getDay();
+  const currentPerson = schedule[currentPersonIndex];
+  const nextPersonIndex = (currentPersonIndex + 1) % schedule.length;
+  const nextPerson = schedule[nextPersonIndex];
 
-  const nomeAtual = localStorage.getItem(storageKey) || nomes[nomes.length - 1];
-  const nomeProximo = nomes[proximaPessoaIndex];
-
-  nomeAtualEl.textContent = `Hoje: ${nomeAtual}`;
-  nomeProximoEl.textContent = `Próximo: ${nomeProximo}`;
-
-  localStorage.setItem(storageKey, nomeProximo);
+  document.getElementById('current-person').textContent = currentPerson;
+  document.getElementById('next-person').textContent = nextPerson;
 }
 
-// Atualiza o escalonamento à meia-noite
-setInterval(() => {
-  const agora = new Date();
-  if (agora.getHours() === 0 && agora.getMinutes() === 0 && agora.getSeconds() === 0) {
-    atualizarEscalonamento();
-  }
-}, 1000);
+function startUpdatingSchedule() {
+  updateSchedule();
+  setInterval(updateSchedule, 86400000); // Atualiza a cada 24 horas (86400000 milissegundos)
+}
+
+startUpdatingSchedule();
+
+
 
